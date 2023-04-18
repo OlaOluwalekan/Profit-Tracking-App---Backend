20c546881d2a18b82f5205ddc1e3890ae94d99ee
OlaOluwalekan <olalekanbello534@gmail.com>
2023-04-18 19:11:15 +0100

    added netlify hosting to cors origin

--- utility/cors.js
+++ utility/cors.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5501',
   'http://localhost:5501',
   'http://localhost:5173',
+  'https://makeenprofittracking.netlify.app',
 ]
 
 const corsOptions = {
